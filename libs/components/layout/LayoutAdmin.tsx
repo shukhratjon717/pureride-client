53842668731cbb12241efbc93c0e6a99ac0845bc
Shukhratjon <shukhratjon0717@gmail.com>
2024-08-27 01:42:38 +0900

    fix: some fixation are done

--- libs/components/layout/LayoutAdmin.tsx
+++ libs/components/layout/LayoutAdmin.tsx
@@ -142,7 +142,7 @@ const withAdminLayout = (Component: ComponentType) => {
 					>
 						<Toolbar sx={{ flexDirection: 'column', alignItems: 'flexStart' }}>
 							<Stack className={'logo-box'}>
-								<img src={'/img/logo/PureRide'} alt={'logo'} />
+								<img src={'/img/logo/PureRide.png'} alt={'logo'} />
 							</Stack>
 
 							<Stack
